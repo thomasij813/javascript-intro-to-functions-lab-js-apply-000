362d5e81b5b5acd38e836c3efcaab8e2ffcb8a74
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:13:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
