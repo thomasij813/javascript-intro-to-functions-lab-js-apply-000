0f5d56083f506e3f4a6447a3a8fd687034caca8d
Thomas Johnson <thomasij813@gmail.com>
2019-06-01 19:17:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper(string))
 }
+
+function sayHiToGrandma(string) {
+  const lowerCaseResponse = 'I can\'t hear you!'
+  const upperCaseResponse = 'YES INDEED!'
+  const loveResponse = 'I love you, too.'
+  
+  if (string.toLowerCase() === string) {
+    return lowerCaseResponse
+  }
+  
+  if (string.toUpperCase() === string) {
+    return upperCaseResponse
+  }
+  
+  if (string === 'I love u, Grandma.') {
+    return loveResponse
+  }
+  
+}
